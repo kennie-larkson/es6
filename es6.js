0d6f66442b5840405973e8920200d71cabf31888
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-18 11:21:28 +0100

    added keyByValue

--- es6.js
+++ es6.js
@@ -22,18 +22,50 @@ for(let k in obj){
     console.log(k);
 }
 
-
-
-// break(exit iteration completely) and continue(skip an iteration level)
-
 let arr = [1,2,3,4,5]
 for(let s in arr){
     console.log(s);
 }
 
-for (let x of arr){
+
+
+// break(exit iteration completely) and continue(skip an iteration level)
+
+let ar = [1,2,3,4,5]
+
+//loop is stopped (exit) when the condittion x==4 is reached
+for (let x of ar){
     if(x == 4){
         break;
     }
     console.log(x)
 }
+
+//loop is skipped at the condittion x==4 to the next iteration
+for (let x of ar){
+    if(x == 4){
+        continue;
+    }
+    console.log(x)
+}
+
+//finding object key by it's value 
+const myObject = {
+    lname : 'Lawal',
+    fname : 'kennie',
+    job : 'developer'
+}
+
+const findKeyByValue = (object,value) =>{
+    for(prop in object){
+        if(Object.hasOwnProperty(prop)){
+            if(object[prop] === value)
+               return prop;
+            
+        }
+    }
+}
+
+let ans = findKeyByValue(myObject,'developer');
+console.log(ans);
+
